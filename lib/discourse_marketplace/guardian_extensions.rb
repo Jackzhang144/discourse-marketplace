720ba2345503ea4f274891313943f327544f1304
jackzhang <zhangyu030123@163.com>
2026-03-13 00:07:22 +0800

    refactor: 移除不再需要的 guardian 方法

--- lib/discourse_marketplace/guardian_extensions.rb
+++ lib/discourse_marketplace/guardian_extensions.rb
@@ -2,17 +2,6 @@
 
 module DiscourseMarketplace
   module GuardianExtensions
-    def can_view_marketplace_contact_info?(topic)
-      return false unless authenticated?
-
-      # 作者可以查看
-      return true if topic.user_id == current_user.id
-      # Staff 可以查看
-      return true if is_staff?
-
-      true
-    end
-
     def can_mark_topic_resolved?(topic)
       return false unless authenticated?
 
@@ -20,15 +9,6 @@ module DiscourseMarketplace
       return true if is_staff?
       # 作者可以标记
       topic.user_id == current_user.id
-    end
-
-    def can_edit_marketplace_contact_info?(topic)
-      return false unless authenticated?
-
-      # 作者可以编辑
-      return true if topic.user_id == current_user.id
-      # Staff 可以编辑
-      is_staff?
     end
   end
 end
