be89c85a477922ef4547bc52b1419c1af3c93069
jackzhang <zhangyu030123@163.com>
2026-03-13 00:36:13 +0800

    fix: 移除对已删除设置的引用

--- lib/discourse_marketplace/topic_view_serializer_extension.rb
+++ lib/discourse_marketplace/topic_view_serializer_extension.rb
@@ -3,21 +3,14 @@
 module DiscourseMarketplace
   module TopicViewSerializerExtension
     def self.prepended(base)
-      base.attributes :can_mark_topic_resolved, :marketplace_resolved_category_id
+      base.attributes :can_mark_topic_resolved
     end
 
     def can_mark_topic_resolved
-      return false unless SiteSetting.marketplace_resolved_category_id.present?
+      return false unless SiteSetting.marketplace_enabled
       return false unless category_enabled?(object.topic.category_id)
 
-      # 已经是已解决分类的帖子不能再次标记
-      return false if object.topic.category_id == SiteSetting.marketplace_resolved_category_id.to_i
-
       scope.can_mark_topic_resolved?(object.topic)
-    end
-
-    def marketplace_resolved_category_id
-      SiteSetting.marketplace_resolved_category_id.to_i
     end
 
     private
