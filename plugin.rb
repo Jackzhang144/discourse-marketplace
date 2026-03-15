442211cdc2c507cc8a95bd8004e73267641e5e3b
jackzhang <zhangyu030123@163.com>
2026-03-15 14:58:56 +0800

    refactor(marketplace): 移除发帖时单独填写联系方式功能
    - 移除 marketplace_contact_info 自定义字段存储
    - 移除 PostsController 扩展，用户直接在帖子中使用 [contact] BBCode
    - 简化插件逻辑，保持联系方式隐私保护功能

--- plugin.rb
+++ plugin.rb
@@ -13,22 +13,16 @@ register_svg_icon "check"
 
 module ::DiscourseMarketplace
   PLUGIN_NAME = "discourse-marketplace"
-  CONTACT_INFO_CUSTOM_FIELD = "marketplace_contact_info"
 end
 
 require_relative "lib/discourse_marketplace/engine"
 require_relative "lib/discourse_marketplace/category_helpers"
 require_relative "lib/discourse_marketplace/guardian_extensions"
 require_relative "lib/discourse_marketplace/topic_view_serializer_extension"
-require_relative "lib/discourse_marketplace/posts_controller_extension"
 
 after_initialize do
   reloadable_patch do
     ::Guardian.prepend(DiscourseMarketplace::GuardianExtensions)
     ::TopicViewSerializer.prepend(DiscourseMarketplace::TopicViewSerializerExtension)
-    ::PostsController.prepend(DiscourseMarketplace::PostsControllerExtension)
   end
-
-  # 注册 Topic 自定义字段
-  register_topic_custom_field_type(DiscourseMarketplace::CONTACT_INFO_CUSTOM_FIELD, :string)
 end
